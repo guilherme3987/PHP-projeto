9a5352f50fadfbab02981ed3edb8cb6245eccd12
Guilherme <76690766+guilherme3987@users.noreply.github.com>
2023-05-13 11:57:46 -0300

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,50 +1,34 @@
-const novaSenha = document.querySelector("#nova_pass");
-const confirmSenha = document.querySelector("#pass");
-const form = document.querySelector("form");
+var btn = document.querySelector('#send');
 
+btn.addEventListener("click",(event)=>{
+    event.preventDefault();
 
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
+    var testeEmail = document.getElementById("email").value;
+    var testePass = document.getElementById("pass").value;
+    var testeNpass = document.getElementById("nova_pass").value;
 
-  var testeEmail = document.getElementById("email").value;
-  var testeNsenha = document.getElementById("nova_pass").value;
-  var testePass = document.getElementById("pass").value;
-
-  if((testeNsenha != testePass ) || (!(EmailValidacao(testeEmail))) || (testeEmail == " ") || (testeNsenha == " ") || (testePass == " ")){
-    alert("Preencha os campos corretamente: Senhas diferentes ou espaços em branco");
-  }else{
-   // teste de requisições e envio de dados para php
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200){
-        alert("Dados Cadastrados");
-      }
-      var formData = new FormData();
-      formData.append("email",testeEmail);
-      formData.append("pass",testePass);
-
-      xhr.open("POST","dados_troca.php",true);
-      xhr.send(formData);
-    }; 
-  }
-
+   
+   // console.log(testeEmail + "Nova senha: " + testeNpass + "Senha: "+testePass);
+   
+   if((testePass === " ") || (testeNpass === " ") || (testeNpass !== testePass)  || (testeEmail === " ") || !(EmailValidacao(testeEmail))){
+    alert("Preencha corretamente");
+    }
+    else{
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                alert("Dados cadastrados");
+            }
+        };
+        var formData = new FormData();
+        formData.append("email",testeEmail);
+        formData.append("pass",testePass);
+        xhr.open("POST","dados_troca.php",true);
+        xhr.send(formData); 
+   }      
 });
 
-
-   /*
-
-   if(((testePass === " ") || (testePass.length <8)) || (testeEmail === " ") || !(EmailValidacao(testeEmail))){
-    alert("Preencha os campos corretamente");
-    return false;
-   }
-   else{
-    alert("Dados enviados");
-    formulario.submit();
-   }
-*/
-
-
-//Validação de email com regex
+//Validação de email com regex (define uma padrão de entrada)
 function EmailValidacao (email){
     const emailRegex = new RegExp(
         // a até z minúsculo,a a z maiusculo e traços seguidos por @ e .com
